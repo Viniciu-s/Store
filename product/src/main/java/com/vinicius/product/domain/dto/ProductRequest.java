4c50d53e5dbd31a3006e6a7db7d9f1caaaccbd2f
Vinicius Vieira <viniciusvsoares00@gmail.com>
2023-11-27 16:36:18 -0300

    creating ProductRequest

---
+++ product/src/main/java/com/vinicius/product/domain/dto/ProductRequest.java
@@ -0,0 +1,7 @@
+package com.vinicius.product.domain.dto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductRequest(UUID id, String name, String category, BigDecimal price, String image, String description) {
+}
