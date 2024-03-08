0862c5d0e7a758d519ded2a0499babc04b9c69dc
Vinicius Vieira <viniciusvsoares00@gmail.com>
2024-03-08 16:33:14 -0300

    adding the brand parameter and the product api dependency

--- stock/src/main/java/com/vinicius/stock/dto/ProductResponse.java
+++ stock/src/main/java/com/vinicius/stock/dto/ProductResponse.java
@@ -1,7 +1,9 @@
 package com.vinicius.stock.dto;
 
+import com.vinicius.product.domain.dto.BrandResponse;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record ProductResponse(UUID id, String name, String category, BigDecimal price, String image, String color, String size, String description) {
+public record ProductResponse(UUID id, String name, String category, BigDecimal price, String image, String color, String size, String description, BrandResponse brand) {
 }
